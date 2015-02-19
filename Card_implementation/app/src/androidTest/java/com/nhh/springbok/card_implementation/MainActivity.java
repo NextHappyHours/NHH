95bc3c313880af1e067411d8268571f14998a21a
dewaelnicolas <nicolasdewael@MacBook-Pro-de-Nicolas-5.local>
2015-02-19 19:48:11 +0100

    début_tut
    http://www.survivingwithandroid.com/2014/11/a-guide-to-android-recyclerv
    iew-cardview.html

---
+++ Card_implementation/app/src/androidTest/java/com/nhh/springbok/card_implementation/MainActivity.java
@@ -0,0 +1,30 @@
+package com.nhh.springbok.card_implementation;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.test.ApplicationTestCase;
+
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class MainActivity extends Activity {
+    public MainActivity() {
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recList = (RecyclerView) findViewById(R.id.cardList);
+        recList.setHasFixedSize(false);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+    }
+
+}
